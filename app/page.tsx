b2b74b9faf136e77986108c50225e33e20acad3c
NataliiaRosol <93214918+NataliiaRosol@users.noreply.github.com>
2025-01-02 14:55:30 +0200

    test commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,8 @@
-import Image from "next/image";
+import Header from "./components/Header";
+
 
 export default function Home() {
   return (
-    <div className="">hello</div>
+    <Header />
   );
 }
